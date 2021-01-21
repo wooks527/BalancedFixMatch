3b7172d0fdb845edb794133f3df984ee0c8e00b7
Songminkee <42332899+Songminkee@users.noreply.github.com>
2021-01-21 14:27:23 +0900

    accum data change

--- train_cxr_supervised_focal.sh
+++ train_cxr_supervised_focal.sh
@@ -4,10 +4,10 @@ epoch=20
 fold=4
 scheduler='step'
 
-for num_labeled in 100 150
+for num_labeled in 25 50 100
 do
 	data_dir="./data/CXR/imbalance_sample/nl$num_labeled"
-	for random_seed in 0 1 2 3 # for random seed
+	for random_seed in 0 1 # for random seed
 	do
 		for batch_size in 16 # 4 6 8
 		do
@@ -19,16 +19,18 @@ do
 							--num_labeled $num_labeled \
 							--scheduler $scheduler \
 							--mu $mu \
+							--focal_loss \
 							--lambda_u 1.0 \
 							--threshold 0.95 \
 							--fold $fold \
+							--lr 0.005 \
 							--epochs $epoch \
 							--batch_size $batch_size \
 							--random_seed $random_seed \
 							--print_to_file \
 							--metric_types acc ppv recall f1 \
 							--dataset_types train test
-			for mu in 1 2 3
+			for mu in 2 3
 			do
 				for lambda_u in 1.0 #0.75 0.5
 				do
@@ -43,6 +45,7 @@ do
 											--num_labeled $num_labeled \
 											--scheduler $scheduler \
 											--mu $mu \
+											--lr 0.005 \
 											--focal_loss \
 											--gamma $gamma \
 											--lambda_u $lambda_u \
